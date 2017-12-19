681b2f419cd4bb33bc942a8c156364834804b34c
krzysztof.drzazga@capgemini.com <krzysztof.drzazga@op.pl>
2017-12-19 12:31:48 +0100

    Initial commit

---
+++ src/test/java/org/kd/FundManagementTests.java
@@ -0,0 +1,14 @@
+package org.kd;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class FundManagementTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
